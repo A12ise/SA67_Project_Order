d84a22f01043c839b4d23c9c7f4e0eee01d2c2da
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 21:41:32 +0700

    Create employee.go

---
+++ Backend/entity/employee.go
@@ -0,0 +1,26 @@
+package entity
+
+import(
+	"gorm.io/gorm"
+) 
+
+type Employee struct{
+	gorm.Model
+	FirstName		string		
+	LastName		string	
+	Email			string	
+	Password		string		
+	Profile   		string 		`gorm:"type:longtext"`
+
+	// FK from Gender
+	GenderID		uint
+	Gender			Gender 		`gorm:"foreignKey: gender_id"`
+	// FK from Position
+	PositionID		uint
+	Position		Position 	`gorm:"foreignKey: position_id"`
+
+	Members			[]Member	`gorm:"foreignKey:employee_id"`
+	Booking 		[]Booking	`gorm:"foreignKey:employee_id"`
+	Order 			[]Order 	`gorm:"foreignKey:employee_id"`
+	Product 		[]Product 	`gorm:"foreignKey:employee_id"`
+}
