5a43a33816851054e900dcd4490219dafdff4a93
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 23:44:29 +0700

    Update order_product.go

--- Backend/controller/order_product.go
+++ Backend/controller/order_product.go
@@ -7,70 +7,57 @@ import (
 	"net/http"
 )
 
-func CreateOrderProducts(c *gin.Context) {
-	var orderproducts entity.Order_Product
+// func CreateOrderProducts(c *gin.Context) {
+// 	var orderproducts entity.Order_Product
 
 	
-	if err := c.ShouldBindJSON(&orderproducts); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+// 	if err := c.ShouldBindJSON(&orderproducts); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
 
-	db := config.DB()
+// 	db := config.DB()
 
-	// ค้นหา product ด้วย id
-	var product entity.Product
-	db.First(&product, orderproducts.ProductID)
-	if product.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
-		return
-	}
+// 	// ค้นหา product ด้วย id
+// 	var product entity.Product
+// 	db.First(&product, orderproducts.ProductID)
+// 	if product.ID == 0 {
+// 		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
+// 		return
+// 	}
 
-	// ค้นหา order ด้วย id
-	var order entity.Order
-	db.First(&order, orderproducts.OrderID)
-	if order.ID == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "order not found"})
-		return
-	}
+// 	// ค้นหา order ด้วย id
+// 	var order entity.Order
+// 	db.First(&order, orderproducts.OrderID)
+// 	if order.ID == 0 {
+// 		c.JSON(http.StatusNotFound, gin.H{"error": "order not found"})
+// 		return
+// 	}
 
-	// สร้าง OrderProduct
-	op := entity.Order_Product{
-		OrderID:   orderproducts.OrderID,
-		ProductID: orderproducts.ProductID,
-		Quantity:  orderproducts.Quantity,
-	}
+// 	// สร้าง OrderProduct
+// 	op := entity.Order_Product{
+// 		OrderID:   orderproducts.OrderID,
+// 		ProductID: orderproducts.ProductID,
+// 		Quantity:  orderproducts.Quantity,
+// 	}
 
-	// บันทึก OrderProduct ลงในฐานข้อมูล
-	if err := db.Create(&op).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{"message": "order product created successfully"})
-}
+// 	// บันทึก OrderProduct ลงในฐานข้อมูล
+// 	if err := db.Create(&op).Error; err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	c.JSON(http.StatusCreated, gin.H{"message": "order product created successfully"})
+// }
 
-func GetOrderProducts(c *gin.Context) {
-	var orderproducts []entity.Order_Product
-
-	db := config.DB()
-	db.Find(&orderproducts)
-	c.JSON(http.StatusOK, &orderproducts)
-}
-
-func GetOrderProductByID(c *gin.Context) {
+func GetOrderProductsByOrderID(c *gin.Context) {
 	ID := c.Param("id")
-	var order entity.Order
+	var orderproduct []entity.Order
  
 	db := config.DB()
-	results := db.First(&order, ID)
-	if results.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": results.Error.Error()})
-		return
-	}
- 
-	if order.ID == 0 {
-		c.JSON(http.StatusNoContent, gin.H{})
-		return
-	}
-	c.JSON(http.StatusOK, order)
+	results := db.Raw("SELECT * FROM Order_Product left join Order on Order.order_id = Order_Product.order_id").Find(&orderproduct, ID)
+	 if results.Error != nil {
+		 c.JSON(http.StatusInternalServerError, gin.H{"error": results.Error.Error()})
+		 return
+	 }
+	 c.JSON(http.StatusOK, orderproduct)
  }
