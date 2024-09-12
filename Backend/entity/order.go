a3186296a055f5a125d524cb5f9332987db34d8f
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 22:07:10 +0700

    Update order.go

--- Backend/entity/order.go
+++ Backend/entity/order.go
@@ -5,17 +5,16 @@ import "gorm.io/gorm"
 
 type Order struct {
 	gorm.Model	
-	Name		string
 
-	BookingID 		*uint `json:"booking_id"`
-	Booking Booking `gorm:"foreignKey:booking_id"`
+	BookingID 		uint `json:"booking_id"`
+	Booking Booking `gorm:"foreignKey:BookingID"`
 
-	EmployeeID 	*uint `json:"employee_id"`
+	EmployeeID 	uint `json:"employee_id"`
 	Employee Employee `gorm:"foreignKey:employee_id"`
 
-	Status_OrderID 		*uint `json:"status_order_id"`
-	Status_Order Status_Order `gorm:"foreignKey:status_order_id"`
+	Status_OrderID 		uint `json:"status_order_id"`
+	Status_Order Status_Order `gorm:"foreignKey:StatusOrderID"`
 
 
-	Order_Product []Order_Product `gorm:"foreignKey:order_id"`
+	Order_Product []Order_Product `gorm:"foreignKey:OrderID"`
 }
