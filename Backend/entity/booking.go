b4e1db028ee9d24fa834d43e3df2fe49ecf94140
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 21:41:29 +0700

    Create booking.go

---
+++ Backend/entity/booking.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+
+    "gorm.io/gorm"
+
+)
+
+type Booking struct {
+
+    gorm.Model
+    NumberOfCustomer  uint      `json:"number_of_customers"`
+
+    PackageID         uint     `json:"package_id"`
+    Package           Package   `gorm:"foreignKey:PackageID"`
+
+    TableID           uint     `json:"table_id"`
+    Table             Table     `gorm:"foreignKey:TableID"`
+
+    MemberID          uint     `json:"member_id"`
+    Member            Member    `gorm:"foreignKey:MemberID"`
+
+    EmployeeID        uint     `json:"employee_id"`
+    Employee          Employee  `gorm:"foreignKey:EmployeeID"`
+    
+}
