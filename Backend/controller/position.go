ddd267d0a93a5a3e83ad4f410e6f25439eaa0123
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 21:46:07 +0700

    Create position.go

---
+++ Backend/controller/position.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+   "net/http"
+   "github.com/A12ise/SA67_Project_Order/config"
+   "github.com/A12ise/SA67_Project_Order/entity"
+   "github.com/gin-gonic/gin"
+)
+
+func GetPositions(c *gin.Context) {
+   var positions []entity.Position
+   
+   db := config.DB()
+   db.Find(&positions)
+   c.JSON(http.StatusOK, &positions)
+}
