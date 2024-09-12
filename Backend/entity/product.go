458e2fbedf99a86aa7d3d965a919833ac32a6644
A12ise <sorrawit456flookza@gmail.com>
2024-09-12 21:41:42 +0700

    Create product.go

---
+++ Backend/entity/product.go
@@ -0,0 +1,18 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Product_code_id   string `json:"product_code_id"`
+
+	Product_name string	`json:"product_name"`
+
+	Category_id  string	`json:"category_id"`
+
+	EmployeeID  *uint	`json:"employee_id"`
+	Employee Employee `gorm:"foreignKey:employee_id"`
+
+	Order_Product []Order_Product `gorm:"foreignKey:product_id"`
+
+}
