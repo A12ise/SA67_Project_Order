530bd1282212bab050f8856963e258ed171fc11d
A12ise <sorrawit456flookza@gmail.com>
2024-09-13 21:03:56 +0700

    Update order_product.go

--- Backend/controller/order_product.go
+++ Backend/controller/order_product.go
@@ -7,47 +7,7 @@ import (
 	"net/http"
 )
 
-// func CreateOrderProducts(c *gin.Context) {
-// 	var orderproducts entity.Order_Product
 
-	
-// 	if err := c.ShouldBindJSON(&orderproducts); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	db := config.DB()
-
-// 	// ค้นหา product ด้วย id
-// 	var product entity.Product
-// 	db.First(&product, orderproducts.ProductID)
-// 	if product.ID == 0 {
-// 		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
-// 		return
-// 	}
-
-// 	// ค้นหา order ด้วย id
-// 	var order entity.Order
-// 	db.First(&order, orderproducts.OrderID)
-// 	if order.ID == 0 {
-// 		c.JSON(http.StatusNotFound, gin.H{"error": "order not found"})
-// 		return
-// 	}
-
-// 	// สร้าง OrderProduct
-// 	op := entity.Order_Product{
-// 		OrderID:   orderproducts.OrderID,
-// 		ProductID: orderproducts.ProductID,
-// 		Quantity:  orderproducts.Quantity,
-// 	}
-
-// 	// บันทึก OrderProduct ลงในฐานข้อมูล
-// 	if err := db.Create(&op).Error; err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusCreated, gin.H{"message": "order product created successfully"})
-// }
 func GetOrderProductsByOrderID(c *gin.Context) {
     // Get the order ID from the URL parameter
     ID := c.Param("id")
